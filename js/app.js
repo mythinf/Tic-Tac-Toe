7d9849792045a70372c78e81370441b03d6befe6
MYTH-INF <maytheinfante96@gmail.com>
2017-11-12 15:10:50 -0500

    agregando función ganador

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
-//
+
+td1 = document.getElementById("td1");
+td2 = document.getElementById("td2");
+td3 = document.getElementById("td3");
+td4 = document.getElementById("td4");
+td5 = document.getElementById("td5");
+td6 = document.getElementById("td6");
+td7 = document.getElementById("td7");
+td8 = document.getElementById("td8");
+td9 = document.getElementById("td9");
+
 window.onload = function() {
   var board = document.querySelector('.board-js');
 
@@ -15,3 +25,22 @@ function addX(event) {
     centinel = !centinel;
   }
 }
+
+function winer(event){
+  if(td1 ==='X'&& td2 ==='X' && td3 === 'X'){
+     var result = document.getElementTagName('td').innerHTML = 'Ganador: X ';
+     alert ('gol');
+  }
+}
+/*  else {
+
+  }
+}*/
+
+//*console.log(winer());*/
+
+  /*else {
+    alert('JUEGA OTRA VEZ');
+    setTimeout("location.href = location.href", 2000);
+  };
+}*/
